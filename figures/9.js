0e64a695ee69f2a64ecb7ad31b698b8a5906d8db
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2020-09-16 15:52:52 +0300

    highlights 12-20

--- figures/9.js
+++ figures/9.js
@@ -12,7 +12,7 @@ import prop8 from '../prose/9/8';
 import prop9 from '../prose/9/9';
 import prop10 from '../prose/9/10';
 import prop11 from '../prose/9/11';
-import prop12 from '../prose/9/11';
+import prop12 from '../prose/9/12';
 import prop13 from '../prose/9/13';
 import prop14 from '../prose/9/14';
 import prop15 from '../prose/9/15';
@@ -22,7 +22,7 @@ import prop18 from '../prose/9/18';
 import prop19 from '../prose/9/19';
 import prop20 from '../prose/9/20';
 import prop21 from '../prose/9/21';
-import prop22 from '../prose/9/21';
+import prop22 from '../prose/9/22';
 import prop23 from '../prose/9/23';
 import prop24 from '../prose/9/24';
 import prop25 from '../prose/9/25';
@@ -32,7 +32,7 @@ import prop28 from '../prose/9/28';
 import prop29 from '../prose/9/29';
 import prop30 from '../prose/9/30';
 import prop31 from '../prose/9/31';
-import prop32 from '../prose/9/31';
+import prop32 from '../prose/9/32';
 import prop33 from '../prose/9/33';
 import prop34 from '../prose/9/34';
 import prop35 from '../prose/9/35';
@@ -203,6 +203,16 @@ function() {
   return {
     title: 'Proposition 12',
     prose: prop12,
+    mags: [
+      { l: 'A', m: 100, p: [50, 50] },
+      { l: 'B', m: 120, v: 40 },
+      { l: 'C', m: 140, v: 40 },
+      { l: 'D', m: 160, v: 40 },
+      { l: 'E', m: 85, p: [250, 50] },
+      { l: 'F', m: 145, v: 40 },
+      { l: 'G', m: 125, v: 40 },
+      { l: 'H', m: 105, v: 40 },
+    ]
   };
 },
 
@@ -210,6 +220,16 @@ function() {
   return {
     title: 'Proposition 13',
     prose: prop13,
+    mags: [
+      { l: 'A', m: 40, p: [50, 50] },
+      { l: 'B', m: 70, v: 40 },
+      { l: 'C', m: 100, v: 40 },
+      { l: 'D', m: 130, v: 40 },
+      { l: 'E', m: 65, p: [250, 50] },
+      { l: 'F', m: 95, v: 40 },
+      { l: 'G', m: 55, v: 40 },
+      { l: 'H', m: 60, v: 40 },
+    ]
   };
 },
 
@@ -217,6 +237,14 @@ function() {
   return {
     title: 'Proposition 14',
     prose: prop14,
+    mags: [
+      { l: 'A', m: 100, p: [50, 50] },
+      { l: 'E', m: 70, v: 40 },
+      { l: 'F', m: 40, v: 40 },
+      { l: 'B', m: 50, p: [200, 50] },
+      { l: 'C', m: 60, v: 40 },
+      { l: 'D', m: 80, v: 40 },
+    ]
   };
 },
 
@@ -224,6 +252,14 @@ function() {
   return {
     title: 'Proposition 15',
     prose: prop15,
+    mags: [
+      { l: 'A', m: 100, p: [50, 50] },
+      { l: 'B', m: 130, v: 40 },
+      { l: 'C', m: 160, v: 40 },
+      { l: 'D', m: 60, p: [250, 50] },
+      { l: 'E', m: 80 },
+      { l: 'F'},
+    ]
   };
 },
 
@@ -231,6 +267,11 @@ function() {
   return {
     title: 'Proposition 16',
     prose: prop16,
+    mags: [
+      { l: 'A', m: 100, p: [50, 50] },
+      { l: 'B', m: 130, v: 40 },
+      { l: 'C', m: 160, v: 40 },
+    ]
   };
 },
 
@@ -238,6 +279,13 @@ function() {
   return {
     title: 'Proposition 17',
     prose: prop17,
+    mags: [
+      { l: 'A', m: 100, p: [50, 50] },
+      { l: 'B', m: 130, v: 40 },
+      { l: 'C', m: 160, v: 40 },
+      { l: 'D', m: 190, v: 40 },
+      { l: 'E', m: 210, v: 40 },
+    ]
   };
 },
 
@@ -245,6 +293,12 @@ function() {
   return {
     title: 'Proposition 18',
     prose: prop18,
+    mags: [
+      { l: 'A', m: 90, p: [50, 50] },
+      { l: 'B', m: 110, v: 40 },
+      { l: 'C', m: 160, p: [200, 50] },
+      { l: 'D', m: 130, v: 40 },
+    ]
   };
 },
 
@@ -252,6 +306,13 @@ function() {
   return {
     title: 'Proposition 19',
     prose: prop19,
+    mags: [
+      { l: 'A', m: 100, p: [50, 50] },
+      { l: 'B', m: 130, v: 40 },
+      { l: 'C', m: 160, v: 40 },
+      { l: 'D', m: 210, v: 40 },
+      { l: 'E', m: 190, v: 40 },
+    ]
   };
 },
 
@@ -259,6 +320,15 @@ function() {
   return {
     title: 'Proposition 20',
     prose: prop20,
+    mags: [
+      { l: 'A', m: 90, p: [50, 50] },
+      { l: 'B', m: 110, v: 40 },
+      { l: 'C', m: 130, v: 40 },
+      { l: 'E', m: 240, v: 40 },
+      { l: 'D', m: 40 },
+      { l: 'F' },
+      { l: 'G', m: 150, p: [230, 50] },
+    ]
   };
 },
 
