82b76e75bff5fcf7b04f30b9f82bd3a83453c424
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2020-09-19 14:38:48 +0300

    terse serve

--- rollup.min.config.js
+++ rollup.min.config.js
@@ -2,31 +2,35 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { string } from "rollup-plugin-string";
 import json from '@rollup/plugin-json';
+import {terser} from 'rollup-plugin-terser';
 
 export default [{
   input: 'main.js',
   plugins: [
     nodeResolve({ preferBuiltins: false }),
     commonjs(),
+    terser(),
   ],
   output: {
     file: 'build/main.js',
     format: 'iife',
-    sourcemap: true,
+    sourcemap: false,
   }
 },
 {
   input: 'books.js',
   plugins: [
+    commonjs(),
     string({
       include: 'prose/**/*'
     }),
-    json()
+    json(),
+    terser()
   ],
   output: {
     file: 'build/books.js',
     format: 'iife',
-    sourcemap: true,
+    sourcemap: false,
   }
 },
 ];
