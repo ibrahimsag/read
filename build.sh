c616a599a9a0fc9064ace73952954e82aaf91088
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2020-09-19 18:17:08 +0300

    build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+rollup -c rollup.prepare.config.js
+node build/prepare.js
+
+rollup -c rollup.min.config.js
+cp build/main.js public/build/ && echo "copied main.js"
+cp build/books.js public/build/ && echo "copied books.js"
+
+sed '/latest.js/d' euclid.html > public/euclid.html && echo "copied euclid.html"
