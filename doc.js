1e5cf1a4ce20293a7355240b72e79fa9d85c8447
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2020-09-25 23:20:54 +0300

    using promises

--- doc.js
+++ doc.js
@@ -19,88 +19,57 @@ function polygon(vs, o)
   return rsvg.generator.polygon(vs, Object.assign(Object.assign({}, roughopts), o));
 }
 
-function rect(r, o)
+function rect(p1, p2, o)
 {
-  return polygon([r[0], [r[1][0], r[0][1]], r[1], [r[0][0], r[1][1]]], o)
+  return polygon([p1, [p2[0], p1[1]], p2, [p1[0], p2[1]]], o)
 }
 
-function load()
-{
-  //
-  // Asynchronous download PDF
-  //
-  var loadingTask = pdfjsLib.getDocument('Elements.pdf');
-  loadingTask.promise.then(function(pdf) {
-    //
-    // Fetch the first page
-    //
-    pdf.getPage(76).then(function(page) {
-      let scale = 1;
-      let viewport = page.getViewport({ scale });
-      let wrapper = document.querySelector('#wrapper');
+//
+// The workerSrc property shall be specified.
+//
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+  'node_modules/pdfjs-dist/build/pdf.worker.js';
 
-      page.getOperatorList().then(opList => {
-        let c = document.createElementNS.bind(document);
-        let nc = (ns, s) => {
-          if(s.startsWith('svg:'))
-            return c(ns, s.slice(4));
-          else
-            return c(ns, s);
-        }
-        document.createElementNS = nc;
+let textLayerEl = document.querySelector('#textLayer');
+let textDivs = [], textContentItemsStr = [];
 
-        const svgGfx = new pdfjsLib.SVGGraphics(page.commonObjs, page.objs);
+let darkMode = svg => {
+  svg.querySelectorAll('*[fill]').forEach(el => {
+    let f = el.getAttribute('fill');
+    if(f === 'rgb(0,0,0)')
+    {
+      el.setAttribute('fill', '#777777');
+    }
 
-        return svgGfx.getSVG(opList, viewport).then(svg => {
-
-          svg.querySelectorAll('*[fill]').forEach(el => {
-
-            let f = el.getAttribute('fill');
-            if(f === 'rgb(0,0,0)')
-            {
-              el.setAttribute('fill', '#777777');
-            }
-
-            let s = el.getAttribute('stroke');
-            if(s === 'rgb(0,0,0)')
-            {
-              el.setAttribute('stroke', '#777777');
-            }
-          });
-
-
-          let textLayerEl = document.querySelector('#textLayer');
-          let textDivs = [], textContentItemsStr = [];
-          page.getTextContent().then(textContent => {
-            pdfjsLib.renderTextLayer({
-              container: textLayerEl,
-              textContent,
-              viewport, textDivs, textContentItemsStr,
-              enhanceTextSelection: true,
-            });
-          });
-
-          wrapper.appendChild(svg);
-        });
-      });
-    });
+    let s = el.getAttribute('stroke');
+    if(s === 'rgb(0,0,0)')
+    {
+      el.setAttribute('stroke', '#777777');
+    }
   });
+  return svg
+};
+
+/* HACK TO KEEP SVG SERIALIZABLE */
+let c = document.createElementNS.bind(document);
+let nc = (ns, s) => {
+  if(s.startsWith('svg:'))
+    return c(ns, s.slice(4));
+  else
+    return c(ns, s);
+}
+document.createElementNS = nc;
+
+let rem = (el) =>
+{
+  let p = el.parentElement;
+  p.removeChild(el);
+  if(!p.children.length)
+    rem(p);
 }
 
-load();
-
-window.getFigure = () =>
+let onlyFigure = (svg) =>
 {
-  let svg = document.querySelector('#wrapper svg');
-  rsvg = rough.svg(svg);
-
-  let rem = (el) =>
-  {
-    let p = el.parentElement;
-    p.removeChild(el);
-    if(!p.children.length)
-      rem(p);
-  }
   svg.querySelectorAll('text').forEach(rem);
   svg.removeChild(svg.children[0]);
   let g = svg.children[0];
@@ -115,19 +84,73 @@ window.getFigure = () =>
   ng.appendChild(g);
   svg.appendChild(ng);
 
-  let bb = ng.getBBox();
-
-  let s = rect([[bb.x, bb.y], [bb.x+bb.width, bb.y+bb.height]], {stroke: colors.make([-30, 100, 40])});
-  svg.appendChild(rsvg.draw(s));
-
-  let vb = [bb.x, bb.y, bb.width, bb.height];
-  svg.setAttribute('width', vb[2]);
-  svg.setAttribute('height', vb[3]);
-  svg.setAttribute('viewBox', vb.join(' '));
+  return ng.getBBox();
 }
 
-//
-// The workerSrc property shall be specified.
-//
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  'node_modules/pdfjs-dist/build/pdf.worker.js';
+function load()
+{
+  //
+  // Asynchronous download PDF
+  //
+  var loadingTask = pdfjsLib.getDocument('Elements.pdf');
+  let wrapper = document.querySelector('#wrapper');
+  let viewport, page;
+  let pagePromise = loadingTask.promise
+    .then(function(pdf)
+    {
+      return pdf.getPage(76);
+    });
+
+  let svgPromise = pagePromise
+    .then(function(page)
+    {
+      let scale = 1;
+      viewport = page.getViewport({ scale });
+
+      return page.getOperatorList()
+        .then(opList =>
+        {
+          const svgGfx = new pdfjsLib.SVGGraphics(page.commonObjs, page.objs);
+
+          return svgGfx.getSVG(opList, viewport);
+        });
+    });
+
+  let textLayerPromise = pagePromise
+    .then(page =>
+    {
+      return page.getTextContent()
+        .then(textContent =>
+        {
+          return pdfjsLib.renderTextLayer({
+            container: textLayerEl,
+            textContent,
+            viewport, textDivs, textContentItemsStr,
+            enhanceTextSelection: true,
+          }).promise;
+        });
+    });
+
+  Promise.all([svgPromise, textLayerPromise]).then(([svg, textLayer]) => {
+
+    wrapper.appendChild(svg);
+    rsvg = rough.svg(svg);
+
+    darkMode(svg);
+    let bb = onlyFigure(svg);
+    let tl = [bb.x, bb.y];
+    let br = [bb.x+bb.width, bb.y+bb.height];
+
+    let s = rect(tl, br, {stroke: colors.make([-30, 100, 40])});
+    svg.appendChild(rsvg.draw(s));
+
+    /*
+    let vb = [bb.x, bb.y, bb.width, bb.height];
+    svg.setAttribute('width', vb[2]);
+    svg.setAttribute('height', vb[3]);
+    svg.setAttribute('viewBox', vb.join(' '));
+    */
+  });
+}
+
+window.onload = load;
